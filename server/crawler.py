fdbe1346c8757c0fd199a79b6e97384e9ce8bf83
Ubuntu <ubuntu@ip-172-31-95-6.ec2.internal>
2022-02-01 18:14:46 +0000

    First test crawler

--- server/crawler.py
+++ server/crawler.py
@@ -1,10 +1,16 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 class Crawler(scrapy.Spider):
 	name = 'crawler'
-	allowed_domains = []
-	start_urls = []
-	def __init__(self, domain, url):
-		allowed_domains.add(domain)
-		start_urls.add(url)
+	allowed_domains = ['crawler-test.com']
+	start_urls = ['https://crawler-test.com/']
+	
+	def parse(self, response):
+		print ("Processing: "+response.url)
+		print (response)
+process = CrawlerProcess()
 
+if __name__ == "__main__":
+	process.crawl(Crawler)
+	process.start()
