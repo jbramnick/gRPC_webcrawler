d623d44274c5cb5aded44917879657116cc57f31
Ubuntu <ubuntu@ip-172-31-95-6.ec2.internal>
2022-02-01 21:53:38 +0000

    added soup crawler and disabled logging for scrapy

--- server/crawler.py
+++ server/crawler.py
@@ -3,11 +3,15 @@ from scrapy.crawler import CrawlerProcess
 
 class Crawler(scrapy.Spider):
 	name = 'crawler'
-	def parse(self, response):
-		print ("Processing: "+response.url)
-		print (response)
-process = CrawlerProcess()
 
+	def parse(self, response):
+		print (response.url)
+		urls = response.css('a::attr(href)').extract()
+		for url in urls:
+			yield response.follow(url, callback=self.parse)
+		
 if __name__ == "__main__":
-	process.crawl(Crawler, start_urls=["https://crawler-test.com/"], allowed_domains = ["https://crawler-test.com/"])
+	Crawler.custom_settings = {'RETRY_TIMES':0,'LOG_ENABLED':False}
+	process = CrawlerProcess()
+	process.crawl(Crawler, start_urls=["https://crawler-test.com/"], allowed_domains = ["crawler-test.com"])
 	process.start()
