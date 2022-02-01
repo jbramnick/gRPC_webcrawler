21a8bfb38a60bc26444563580c75c45ef25d035f
Ubuntu <ubuntu@ip-172-31-95-6.ec2.internal>
2022-02-01 19:23:51 +0000

    parameterized crawler

--- server/crawler.py
+++ server/crawler.py
@@ -3,14 +3,11 @@ from scrapy.crawler import CrawlerProcess
 
 class Crawler(scrapy.Spider):
 	name = 'crawler'
-	allowed_domains = ['crawler-test.com']
-	start_urls = ['https://crawler-test.com/']
-	
 	def parse(self, response):
 		print ("Processing: "+response.url)
 		print (response)
 process = CrawlerProcess()
 
 if __name__ == "__main__":
-	process.crawl(Crawler)
+	process.crawl(Crawler, start_urls=["https://crawler-test.com/"], allowed_domains = ["https://crawler-test.com/"])
 	process.start()
