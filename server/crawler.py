1b7aaf8c06f9e73c5a42c63adfc759d505bfe6df
Ubuntu <ubuntu@ip-172-31-95-6.ec2.internal>
2022-02-01 17:20:02 +0000

    confirming git changes worked

--- server/crawler.py
+++ server/crawler.py
@@ -3,6 +3,8 @@ import scrapy
 class Crawler(scrapy.Spider):
 	name = 'crawler'
 	allowed_domains = []
+	start_urls = []
 	def __init__(self, domain, url):
 		allowed_domains.add(domain)
+		start_urls.add(url)
 
